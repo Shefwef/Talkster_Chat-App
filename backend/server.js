2f16cedfb95b8022003f805dd43eb0675c4b689a
Shefwef <shefayatadib@iut-dhaka.edu>
2024-07-12 01:14:03 +0600

    Signup and some of the backend parts

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import connectToMongoDB from "./database/connectToMongoDB.js";
 
-dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  // root route http://localhost:5000/
-  res.send("Server is ready!!");
-});
+dotenv.config();
+
+app.use(express.json()); // to parse the incloming requests with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+// app.get("/", (req, res) => {
+//   // root route http://localhost:5000/
+//   res.send("Server is ready!!");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
+});
