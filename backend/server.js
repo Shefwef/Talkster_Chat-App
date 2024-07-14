02b6764305db9491df761df25392168a5dcd657c
Shefwef <shefayatadib@iut-dhaka.edu>
2024-07-15 02:19:42 +0600

    Message being sent successfully from sender to receiver with the time of sending

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./database/connectToMongoDB.js";
 
 const app = express();
@@ -9,8 +12,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // to parse the incloming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // app.get("/", (req, res) => {
 //   // root route http://localhost:5000/
