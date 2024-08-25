530929bc2719a6515188f2bb86ced333c0f92763
Shefwef <shefayatadib@iut-dhaka.edu>
2024-08-26 03:41:03 +0600

    Finished the signup and the login functionalities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import "./App.css";
-import Login from "./pages/login/Login.jsx";
-import Signup from "./pages/signup/SignUp.jsx";
 import Home from "./pages/home/Home.jsx";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      {/* <Signup /> */}
       <Home />
     </div>
   );
